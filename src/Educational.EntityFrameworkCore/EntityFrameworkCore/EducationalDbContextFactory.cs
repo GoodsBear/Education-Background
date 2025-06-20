76c235554e525004d579edd557a1cf469bdc2488
unknown <2754590762@qq.com>
2025-06-20 18:53:21 +0800

    Initial commit

---
+++ src/Educational.EntityFrameworkCore/EntityFrameworkCore/EducationalDbContextFactory.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.IO;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.Configuration;
+
+namespace Educational.EntityFrameworkCore;
+
+/* This class is needed for EF Core console commands
+ * (like Add-Migration and Update-Database commands) */
+public class EducationalDbContextFactory : IDesignTimeDbContextFactory<EducationalDbContext>
+{
+    public EducationalDbContext CreateDbContext(string[] args)
+    {
+        EducationalEfCoreEntityExtensionMappings.Configure();
+
+        var configuration = BuildConfiguration();
+
+        var builder = new DbContextOptionsBuilder<EducationalDbContext>()
+            .UseMySql(configuration.GetConnectionString("Default"),ServerVersion.Parse("5.7.22-mysql"));
+
+        return new EducationalDbContext(builder.Options);
+    }
+
+    private static IConfigurationRoot BuildConfiguration()
+    {
+        var builder = new ConfigurationBuilder()
+            .SetBasePath(Path.Combine(Directory.GetCurrentDirectory(), "../Educational.DbMigrator/"))
+            .AddJsonFile("appsettings.json", optional: false);
+
+        return builder.Build();
+    }
+}
---
+++ src/Educational.EntityFrameworkCore/Migrations/20250620090254_初始化数据.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Educational.Migrations
+{
+    /// <inheritdoc />
+    public partial class 初始化数据 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
